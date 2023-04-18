729796f760afbacd94ec659390ddeb162bb0b249
gbudau <54190400+gbudau@users.noreply.github.com>
2023-04-18 23:25:05 +0200

    Add Wordpress Dockerfile

---
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env ash
+
+# Wait until mysql is up
+until wp db check --path="/var/www/html"; do echo waiting for MariaDB; sleep 2; done
+
+# If wordpress is not installed, install it
+if ! $(wp core is-installed --path=/var/www/html); then
+	wp core install --path=/var/www/html --url="$WORDPRESS_URL" --title="$WORDPRESS_SITE_TITLE" --admin_email="$WORDPRESS_ADMIN_EMAIL" --admin_user="$WORDPRESS_ADMIN" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --skip-email
+	wp user create "$WORDPRESS_EDITOR" "$WORDPRESS_EDITOR_EMAIL" --user_pass="$WORDPRESS_EDITOR_PASSWORD" --role=editor --path=/var/www/html
+fi
+
+exec /usr/sbin/php-fpm -F
