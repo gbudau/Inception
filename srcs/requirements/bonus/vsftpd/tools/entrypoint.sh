f4c81d9e34b6bdfedfadecc0204f9aab81a35f7b
gbudau <54190400+gbudau@users.noreply.github.com>
2023-05-13 18:19:58 +0200

    Rename the wordpress ftp directory
    Rename the Wordpress FTP directory to wordpress from files

--- srcs/requirements/bonus/vsftpd/tools/entrypoint.sh
+++ srcs/requirements/bonus/vsftpd/tools/entrypoint.sh
@@ -3,9 +3,9 @@
 adduser -D -h "/home/$FTPS_USER" -s /sbin/nologin "$FTPS_USER"
 echo "$FTPS_USER:$FTPS_USER_PASSWORD" | chpasswd
 mkdir -p /var/run/vsftpd/empty
-mkdir -p /vsftpd/"$FTPS_USER"/ftp/files
+mkdir -p /vsftpd/"$FTPS_USER"/ftp/wordpress
 chown nobody:nogroup /vsftpd/"$FTPS_USER"/ftp
 chmod a-w /vsftpd/"$FTPS_USER"/ftp
-chown "$FTPS_USER:$FTPS_USER" /vsftpd/"$FTPS_USER"/ftp/files
+chown "$FTPS_USER:$FTPS_USER" /vsftpd/"$FTPS_USER"/ftp/wordpress
 echo "$FTPS_USER" > /etc/vsftpd.chroot_list
 /usr/sbin/vsftpd "-opasv_address=$FTPS_PASV_ADDRESS" /etc/vsftpd/vsftpd.conf
