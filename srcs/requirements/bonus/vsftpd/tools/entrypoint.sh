93bc7aaa36cc202bf2a7062b9522b0c50eab7602
gbudau <54190400+gbudau@users.noreply.github.com>
2023-05-13 21:43:31 +0200

    Update alpine version to 3.17
    Create a shared group for ftp and wordpress
    Add SHARED_GROUP, SHARED_UID and SHARED_GID environment variables
    for setting the wordpress volume permissions

--- srcs/requirements/bonus/vsftpd/tools/entrypoint.sh
+++ srcs/requirements/bonus/vsftpd/tools/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env ash
 
-adduser -D -h "/home/$FTPS_USER" -s /sbin/nologin "$FTPS_USER"
+addgroup -g "${SHARED_GID}" "${SHARED_GROUP}"
+adduser -D -h "/home/$FTPS_USER" -s /sbin/nologin -u "$SHARED_UID" -G "$SHARED_GROUP" "$FTPS_USER"
 echo "$FTPS_USER:$FTPS_USER_PASSWORD" | chpasswd
 mkdir -p /var/run/vsftpd/empty
 mkdir -p /vsftpd/"$FTPS_USER"/ftp/wordpress
 chown nobody:nogroup /vsftpd/"$FTPS_USER"/ftp
 chmod a-w /vsftpd/"$FTPS_USER"/ftp
-chown "$FTPS_USER:$FTPS_USER" /vsftpd/"$FTPS_USER"/ftp/wordpress
+chown -R "$SHARED_UID:$SHARED_GID" /vsftpd/"$FTPS_USER"/ftp/wordpress
 echo "$FTPS_USER" > /etc/vsftpd.chroot_list
 /usr/sbin/vsftpd "-opasv_address=$FTPS_PASV_ADDRESS" /etc/vsftpd/vsftpd.conf
