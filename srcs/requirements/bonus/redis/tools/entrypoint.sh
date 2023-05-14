324b9e5b1cd4269cec5a68b8c0fa45c0e6eec40f
gbudau <54190400+gbudau@users.noreply.github.com>
2023-05-14 17:28:38 +0200

    Add redis service
    Enable Redis Object Cache in Wordpress

---
+++ srcs/requirements/bonus/redis/tools/entrypoint.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env ash
+
+exec /usr/bin/redis-server /etc/redis.conf
--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -7,8 +7,12 @@ until wp db check --path="/var/www/html"; do echo waiting for MariaDB; sleep 2; 
 
 # If wordpress is not installed, install it
 if ! $(wp core is-installed --path="/var/www/html"); then
-	wp core install --path="/var/www/html" --url="$WORDPRESS_URL" --title="$WORDPRESS_SITE_TITLE" --admin_email="$WORDPRESS_ADMIN_EMAIL" --admin_user="$WORDPRESS_ADMIN" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --skip-email
+	wp core install --path="/var/www/html" --url="https://$WORDPRESS_DOMAIN" --title="$WORDPRESS_SITE_TITLE" --admin_email="$WORDPRESS_ADMIN_EMAIL" --admin_user="$WORDPRESS_ADMIN" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --skip-email
 	wp user create "$WORDPRESS_EDITOR" "$WORDPRESS_EDITOR_EMAIL" --user_pass="$WORDPRESS_EDITOR_PASSWORD" --role="editor" --path="/var/www/html"
+	wp plugin install --path="/var/www/html" redis-cache --activate
+	wp config set WP_REDIS_HOST redis --path="/var/www/html"
+	wp config set WP_REDIS_PORT 6379 --raw --path="/var/www/html"
+	wp redis enable --path="/var/www/html"
 fi
 
 exec /usr/sbin/php-fpm -F
